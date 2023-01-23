f6209a2ae8e29b41c176010f71b4e516c872bd4a
sai-rocky <74547941+sai-rocky@users.noreply.github.com>
2023-01-23 09:16:26 +0530

    Initial commit

---
+++ src/main/java/com/shopup/Geometry/Geometry.java
@@ -0,0 +1,5 @@
+package com.shopup.Geometry;
+
+public class Geometry {
+
+}
---
+++ src/test/java/com/shopup/Geometry/GeometryTest.java
@@ -0,0 +1,7 @@
+package com.shopup.Geometry;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeometryTest {
+
+}
